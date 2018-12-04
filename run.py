219afb60cbfec992335b528c1945f844849cdf19
Vladislav Sytchenko <sytchenk@dh2020pc30.utm.utoronto.ca>
2018-12-03 23:39:44 -0500

    [A3] Update run script

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
+import os
 import subprocess
 
 if __name__ == "__main__":
+    os.remove("./server_0.log")
+    os.remove("./server_1.log")
+    os.remove("./server_2.log")
+
     #./mserver -c 1111 -s 2222 -C ./srvcfg_local.txt
     mserver = subprocess.Popen(["./mserver",
                                 "-c", "1111",
                                 "-s", "2222",
                                 "-C", "./srvcfg_local.txt"])
 
-    #./client -h localhost -p 1111 -f ./ops_sample.txt
-    client = subprocess.Popen(["./client",
-                               "-h", "localhost",
-                               "-p", "1111",
-                               "-f", "./ops_sample.txt"])
+    while True:
+        #./client -h localhost -p 1111 -f ./ops_sample.txt
+        client = subprocess.Popen(["./client",
+                                "-h", "localhost",
+                                "-p", "1111",
+                                "-f", "./ops_sample.txt"])
+        client.wait()
+        break
 
     mserver.wait()
-    client.wait()
